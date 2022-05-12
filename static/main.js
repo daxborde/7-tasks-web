501c8db213a45087ca8ff174346fdabb7fcf5dd1
Dax Borde <daxborde+github@gmail.com>
2022-05-12 14:36:54 -0700

    Implement Counter logic in js

--- static/main.js
+++ static/main.js
@@ -1,8 +1,18 @@
-const incrButton = document.querySelector('button.increment-button');
-const countDisplay = document.querySelector('p.count-display');
+const incrButton = document.querySelector('.increment-button');
+const countDisplay = document.querySelector('.count-display');
+
+// Define global state
+let count = 0;
+
+// Initial render
+countDisplay.textContent = count;
 
 function increment() {
-    console.log("Increment called.")
+    // Change global state
+    count += 1;
+
+    // Re-render based on global state
+    countDisplay.textContent = count;
 }
 
 incrButton.addEventListener('click', increment);
